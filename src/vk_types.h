4d1f48b0e6bb2b921daed1339df355168f25e784
Vishwah <vishwahsivagurunathan@gmail.com>
2025-10-09 14:19:06 +0530

    using seprate image to draw and the copying it to swapchain

--- src/vk_types.h
+++ src/vk_types.h
@@ -29,3 +29,12 @@
             abort();                                                    \
         }                                                               \
     } while (0)
+
+struct AllocatedImage
+{
+	VkImage       image;
+	VkImageView   imageView;
+	VmaAllocation allocation;
+	VkExtent3D    imageExtent;
+	VkFormat      imageFormat;
+};
