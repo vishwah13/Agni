bdb7aa5b8311401d4d0a24b3b95f76da57ffb1c1
Vishwah <vishwahsivagurunathan@gmail.com>
2025-11-19 20:57:44 +0530

    Material related stuff is encapsulated in Material .cpp file

---
+++ src/Material.cpp
@@ -0,0 +1,157 @@
+#include <Material.hpp>
+
+#include <AgniEngine.hpp>
+#include <Initializers.hpp>
+#include <Pipelines.hpp>
+#include <VulkanTools.hpp>
+
+#include <fmt/core.h>
+
+void GltfPbrMaterial::buildPipelines(AgniEngine* engine)
+{
+	VkShaderModule meshFragShader;
+	if (!vkutil::loadShaderModule(
+	    "../../shaders/glsl/mesh.frag.spv", engine->m_device, &meshFragShader))
+	{
+		fmt::println("Error when building the triangle fragment shader module");
+	}
+
+	VkShaderModule meshVertexShader;
+	if (!vkutil::loadShaderModule("../../shaders/glsl/mesh.vert.spv",
+	                              engine->m_device,
+	                              &meshVertexShader))
+	{
+		fmt::println("Error when building the triangle vertex shader module");
+	}
+
+	VkPushConstantRange matrixRange {};
+	matrixRange.offset     = 0;
+	matrixRange.size       = sizeof(GPUDrawPushConstants);
+	matrixRange.stageFlags = VK_SHADER_STAGE_VERTEX_BIT;
+
+	DescriptorLayoutBuilder layoutBuilder;
+	layoutBuilder.addBinding(0, VK_DESCRIPTOR_TYPE_UNIFORM_BUFFER);
+	layoutBuilder.addBinding(1, VK_DESCRIPTOR_TYPE_COMBINED_IMAGE_SAMPLER);
+	layoutBuilder.addBinding(2, VK_DESCRIPTOR_TYPE_COMBINED_IMAGE_SAMPLER);
+	layoutBuilder.addBinding(3, VK_DESCRIPTOR_TYPE_COMBINED_IMAGE_SAMPLER);
+	layoutBuilder.addBinding(4, VK_DESCRIPTOR_TYPE_COMBINED_IMAGE_SAMPLER);
+
+	m_materialLayout = layoutBuilder.build(engine->m_device,
+	                                       VK_SHADER_STAGE_VERTEX_BIT |
+	                                       VK_SHADER_STAGE_FRAGMENT_BIT);
+
+	VkDescriptorSetLayout layouts[] = {engine->m_gpuSceneDataDescriptorLayout,
+	                                   m_materialLayout};
+
+	VkPipelineLayoutCreateInfo mesh_layout_info =
+	vkinit::pipelineLayoutCreateInfo();
+	mesh_layout_info.setLayoutCount         = 2;
+	mesh_layout_info.pSetLayouts            = layouts;
+	mesh_layout_info.pPushConstantRanges    = &matrixRange;
+	mesh_layout_info.pushConstantRangeCount = 1;
+
+	VkPipelineLayout newLayout;
+	VK_CHECK(vkCreatePipelineLayout(
+	engine->m_device, &mesh_layout_info, nullptr, &newLayout));
+
+	m_opaquePipeline.m_layout      = newLayout;
+	m_transparentPipeline.m_layout = newLayout;
+
+	// build the stage-create-info for both vertex and fragment stages. This
+	// lets the pipeline know the shader modules per stage
+	PipelineBuilder pipelineBuilder;
+	pipelineBuilder.setShaders(meshVertexShader, meshFragShader);
+	pipelineBuilder.setInputTopology(VK_PRIMITIVE_TOPOLOGY_TRIANGLE_LIST);
+	pipelineBuilder.setPolygonMode(VK_POLYGON_MODE_FILL);
+	pipelineBuilder.setCullMode(VK_CULL_MODE_FRONT_BIT,
+	                            VK_FRONT_FACE_CLOCKWISE);
+	pipelineBuilder.enableMultisampling(engine->m_msaaSamples);
+	pipelineBuilder.disableBlending();
+	pipelineBuilder.enableDepthtest(true, VK_COMPARE_OP_GREATER_OR_EQUAL);
+
+	// render format
+	pipelineBuilder.setColorAttachmentFormat(
+	engine->m_msaaColorImage.m_imageFormat);
+	pipelineBuilder.setDepthFormat(engine->m_depthImage.m_imageFormat);
+
+	// use the triangle layout we created
+	pipelineBuilder.m_pipelineLayout = newLayout;
+
+	// finally build the pipeline
+	m_opaquePipeline.m_pipeline =
+	pipelineBuilder.buildPipeline(engine->m_device);
+
+	// create the transparent variant
+	pipelineBuilder.enableBlendingAdditive();
+	// turning off depth buffer writes for transparent objects
+	pipelineBuilder.enableDepthtest(false, VK_COMPARE_OP_GREATER_OR_EQUAL);
+
+	m_transparentPipeline.m_pipeline =
+	pipelineBuilder.buildPipeline(engine->m_device);
+
+	vkDestroyShaderModule(engine->m_device, meshFragShader, nullptr);
+	vkDestroyShaderModule(engine->m_device, meshVertexShader, nullptr);
+}
+
+void GltfPbrMaterial::clearResources(VkDevice device)
+{
+	vkDestroyDescriptorSetLayout(device, m_materialLayout, nullptr);
+	vkDestroyPipelineLayout(device, m_transparentPipeline.m_layout, nullptr);
+
+	vkDestroyPipeline(device, m_transparentPipeline.m_pipeline, nullptr);
+	vkDestroyPipeline(device, m_opaquePipeline.m_pipeline, nullptr);
+}
+
+MaterialInstance
+GltfPbrMaterial::writeMaterial(VkDevice                     device,
+                               MaterialPass                 pass,
+                               const MaterialResources&     resources,
+                               DescriptorAllocatorGrowable& descriptorAllocator)
+{
+	MaterialInstance matData;
+	matData.m_passType = pass;
+	if (pass == MaterialPass::Transparent)
+	{
+		matData.m_pipeline = &m_transparentPipeline;
+	}
+	else
+	{
+		matData.m_pipeline = &m_opaquePipeline;
+	}
+
+	matData.m_materialSet =
+	descriptorAllocator.allocate(device, m_materialLayout);
+
+
+	m_writer.clear();
+	m_writer.writeBuffer(/*binding*/ 0,
+	                     resources.m_dataBuffer,
+	                     sizeof(MaterialConstants),
+	                     resources.m_dataBufferOffset,
+	                     VK_DESCRIPTOR_TYPE_UNIFORM_BUFFER);
+	m_writer.writeImage(/*binding*/ 1,
+	                    resources.m_colorImage.m_imageView,
+	                    resources.m_colorSampler,
+	                    VK_IMAGE_LAYOUT_SHADER_READ_ONLY_OPTIMAL,
+	                    VK_DESCRIPTOR_TYPE_COMBINED_IMAGE_SAMPLER);
+	m_writer.writeImage(/*binding*/ 2,
+	                    resources.m_metalRoughImage.m_imageView,
+	                    resources.m_metalRoughSampler,
+	                    VK_IMAGE_LAYOUT_SHADER_READ_ONLY_OPTIMAL,
+	                    VK_DESCRIPTOR_TYPE_COMBINED_IMAGE_SAMPLER);
+	m_writer.writeImage(/*binding*/ 3,
+	                    resources.m_normalImage.m_imageView,
+	                    resources.m_normalSampler,
+	                    VK_IMAGE_LAYOUT_SHADER_READ_ONLY_OPTIMAL,
+	                    VK_DESCRIPTOR_TYPE_COMBINED_IMAGE_SAMPLER);
+	m_writer.writeImage(/*binding*/ 4,
+	                    resources.m_aoImage.m_imageView,
+	                    resources.m_aoSampler,
+	                    VK_IMAGE_LAYOUT_SHADER_READ_ONLY_OPTIMAL,
+	                    VK_DESCRIPTOR_TYPE_COMBINED_IMAGE_SAMPLER);
+
+	// use the materialSet and update it here.
+	m_writer.updateSet(device, matData.m_materialSet);
+
+	return matData;
+}
---
+++ src/Material.hpp
@@ -0,0 +1,50 @@
+#pragma once
+
+#include <Descriptors.hpp>
+#include <Types.hpp>
+
+#include <vk_mem_alloc.h>
+
+// Forward declaration
+class AgniEngine;
+
+struct GltfPbrMaterial
+{
+	MaterialPipeline m_opaquePipeline;
+	MaterialPipeline m_transparentPipeline;
+
+	VkDescriptorSetLayout m_materialLayout;
+
+	struct MaterialConstants
+	{
+		glm::vec4 m_colorFactors;
+		glm::vec4 m_metal_rough_factors;
+		// padding, we need it anyway for uniform buffers
+		glm::vec4 extra[14];
+	};
+
+	struct MaterialResources
+	{
+		AllocatedImage m_colorImage;
+		VkSampler      m_colorSampler;
+		AllocatedImage m_metalRoughImage;
+		VkSampler      m_metalRoughSampler;
+		AllocatedImage m_normalImage;
+		VkSampler      m_normalSampler;
+		AllocatedImage m_aoImage;
+		VkSampler      m_aoSampler;
+		VkBuffer       m_dataBuffer;
+		uint32_t       m_dataBufferOffset;
+	};
+
+	DescriptorWriter m_writer;
+
+	void buildPipelines(AgniEngine* engine);
+	void clearResources(VkDevice device);
+
+	MaterialInstance
+	writeMaterial(VkDevice                     device,
+	              MaterialPass                 pass,
+	              const MaterialResources&     resources,
+	              DescriptorAllocatorGrowable& descriptorAllocator);
+};
