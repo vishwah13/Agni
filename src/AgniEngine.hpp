d0280e73eb1f6b494ac6541373f408f7d786d1b2
Vishwah <vishwahsivagurunathan@gmail.com>
2025-11-19 20:57:44 +0530

    Material related stuff is encapsulated in Material .cpp file

--- src/AgniEngine.hpp
+++ src/AgniEngine.hpp
@@ -6,6 +6,7 @@
 #include <Camera.hpp>
 #include <Descriptors.hpp>
 #include <Loader.hpp>
+#include <Material.hpp>
 #include <ResourceManager.hpp>
 #include <Scene.hpp>
 #include <Skybox.hpp>
@@ -62,47 +63,6 @@ struct ComputeEffect
 	VkPipelineLayout m_layout;
 
 	ComputePushConstants m_data;
-};
-
-struct GltfPbrMaterial
-{
-	MaterialPipeline m_opaquePipeline;
-	MaterialPipeline m_transparentPipeline;
-
-	VkDescriptorSetLayout m_materialLayout;
-
-	struct MaterialConstants
-	{
-		glm::vec4 m_colorFactors;
-		glm::vec4 m_metal_rough_factors;
-		// padding, we need it anyway for uniform buffers
-		glm::vec4 extra[14];
-	};
-
-	struct MaterialResources
-	{
-		AllocatedImage m_colorImage;
-		VkSampler      m_colorSampler;
-		AllocatedImage m_metalRoughImage;
-		VkSampler      m_metalRoughSampler;
-		AllocatedImage m_normalImage;
-		VkSampler      m_normalSampler;
-		AllocatedImage m_aoImage;
-		VkSampler      m_aoSampler;
-		VkBuffer       m_dataBuffer;
-		uint32_t       m_dataBufferOffset;
-	};
-
-	DescriptorWriter m_writer;
-
-	void buildPipelines(AgniEngine* engine);
-	void clearResources(VkDevice device);
-
-	MaterialInstance
-	writeMaterial(VkDevice                     device,
-	              MaterialPass                 pass,
-	              const MaterialResources&     resources,
-	              DescriptorAllocatorGrowable& descriptorAllocator);
 };
 
 class MeshNode : public Node
